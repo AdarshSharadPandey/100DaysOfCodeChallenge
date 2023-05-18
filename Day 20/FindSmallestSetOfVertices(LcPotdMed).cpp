b31d3fed8a219b2ef6224b575d14d2f49a5df98f
Adarsh Sharad Pandey <93065535+AdarshSharadPandey@users.noreply.github.com>
2023-05-18 20:07:16 +0530

    Add files via upload

---
+++ Day 20/FindSmallestSetOfVertices(LcPotdMed).cpp
@@ -0,0 +1,8 @@
+vector<int> findSmallestSetOfVertices(int n, vector<vector<int>>& edges) 
+{
+    vector<int> v(n,0);
+    vector<int> ans;
+    for(int i=0;i<edges.size();i++) v[edges[i][1]]++;
+    for(int i=0;i<n;i++) if(v[i]==0) ans.push_back(i);
+    return ans;
+}
