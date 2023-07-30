fee61e61f339ebb9b5e351d048374c1e1b5a38e0
Adarsh Sharad Pandey <93065535+AdarshSharadPandey@users.noreply.github.com>
2023-07-30 17:12:57 +0530

    Add files via upload

---
+++ Day 81/InorderSuccessor(gfgPotdEasy).cpp
@@ -0,0 +1,28 @@
+Node * inOrderSuccessor(Node *root, Node *x)
+{
+    Node * curr=root,*succ=nullptr;
+    if(x->right)
+    {
+        curr=x;
+        curr=curr->right;
+        while(curr &&curr->left) curr=curr->left;
+        return curr;
+    }
+    else
+    {
+        while(curr)
+        {
+            if(x->data<curr->data)
+            {
+                succ=curr;
+                curr=curr->left;
+            }
+            else if ( x->data>curr->data)
+                curr=curr->right;
+            else 
+                break;
+        }
+        return succ;
+    }
+    
+}
